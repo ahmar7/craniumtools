d5375d24c821d9fe9fb2232f0c741d575427c014
Ahmar Jabbar <87254977+ahmar7@users.noreply.github.com>
2022-09-30 21:41:03 +0500

    content update

---
+++ src/layout/Header/header.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Logo } from '../../utils/allImgs';
+import { Link } from 'react-router-dom';
+import "./header.css"
+const Header = () => {
+    return (
+        <div className='main-container'>
+             <div className="header">
+                <div className="left-colmn">
+                    <ul className='nav-ul'>
+                        <li><Link to=""><img src={Logo} alt="" /></Link></li>
+                        <li><Link to="">Home</Link></li>
+                        <li><Link to="">Experiments</Link></li>
+                        <li><Link to="">Contacts</Link></li>
+                    </ul>
+                </div>
+                <div className="right-colmn">
+                    <button className='panel-btn'>Project Panel</button>
+                </div>
+             </div>
+        </div>
+    );
+}
+
+export default Header;
---
+++ src/utils/allImgs/index.js
@@ -0,0 +1,6 @@
+import Logo from "../../assets/img/Logo.png";
+ 
+
+///////////////////////////////////////////////////// Collection Images
+export { Logo };
+ 
